50c9a189865dfd3fc53cf5cb70214e82ce6c8f53
dipayansaha2002 <sahadipayan36@gmail.com>
2024-09-19 20:19:48 +0530

    Initial commit

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -7,7 +7,7 @@ const Card = ({id, title, tag, status}) => {
         <div className="cardHeading flex-sb">
             <span style={{textTransform : "uppercase"}} className='color-grey'>{id}</span>
             <div className="imageContainer relative" style={{ width : "30px", height : "30px"}}>
-                <img style={{width : "100%", height : "100%",  borderRadius : "50%" }}  src="https://images.unsplash.com/photo-1633332755192-727a05c4013d?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dXNlcnxlbnwwfHwwfHx8MA%3D%3D&w=1000&q=80" alt="UserImage" />
+                <img style={{width : "100%", height : "100%",  borderRadius : "50%" }}  src="https://t4.ftcdn.net/jpg/02/29/75/83/240_F_229758328_7x8jwCwjtBMmC6rgFzLFhZoEpLobB6L8.jpg" alt="UserImage" />
                 <div className="showStatus"></div>
             </div>
         </div>
